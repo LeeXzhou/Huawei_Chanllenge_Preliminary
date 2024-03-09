780feae77ccb84c82a1ba3a8093a027fdb0c77c1
LeeXzhou <jumkyjumky@163.com>
2024-03-09 20:35:12 +0800

    change extern into static

--- project/h_folder/my_algorithm.h
+++ project/h_folder/my_algorithm.h
@@ -2,7 +2,7 @@
 #define my_algorithm_h
 #include "record.h"
 using namespace std;
-extern pair<int, int> pre[205][205], nxt[205][205];
+static pair<int, int> pre[205][205], nxt[205][205];
 /*
 重载pair加减法
 */
@@ -16,7 +16,7 @@ template <class T, class U>
 ostream& operator<<(std::ostream& os, const std::pair<T, U>& p);
 
 namespace my_alg {
-	pair<int, int> dx_dy[4] = { {0, 1} , {0, -1}, {-1, 0}, {1, 0 } };
+	static pair<int, int> dx_dy[4] = { {0, 1} , {0, -1}, {-1, 0}, {1, 0 } };
 	void BFS();
 	bool check_robot(int x, int y); //判断一个位置是否可以让机器人站（合法且不是障碍物或者海），true为可以到达
 	//void init_dis(); // 求出每个点到10个港口的最短距离
